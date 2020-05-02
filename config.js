b6bfe284bca349196d1778fea5d59d5e7ecbe6b3
Amin Shah Gilani <amin@gilani.me>
2020-05-03 04:44:01 +0500

    Update personal information

--- config.js
+++ config.js
@@ -5,21 +5,21 @@ module.exports = {
   description: "Amin Shah Gilani's blog",
   // a short bio shown at the bottom of your blog posts
   // It should complete the sentence: Written by Amin Shah Gilani ...
-  shortBio: '',
+  shortBio: 'Amin is startup interim-CTO, a part-time grad student at Georgia Tech, former startup CTO & CEO, and former editor at Toptal\'s engineering blog',
   // a longer bio showing on the landing page of the blog
-  bio: `Founder. Developer. Spaces over tabs. Atom over your favorite X.`,
+  bio: `Amin is startup interim-CTO, a part-time grad student at Georgia Tech, former startup CTO & CEO, and former editor at Toptal\'s engineering blog`,
   author: 'Amin Shah Gilani',
   githubUrl: 'https://github.com/amingilani/blog',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'https://amin.gilani.me/blog',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: 'amingilani',
     medium: '@gilani',
-    facebook: '',
-    github: '',
-    linkedin: '',
-    instagram: '',
+    facebook: 'aminshahgilani',
+    github: 'amingilani',
+    linkedin: 'amingilani',
+    instagram: 'amingilani',
   },
   // GoatCounter code to enable analytics. See https://github.com/mathieudutour/medium-to-own-blog/tree/master/docs#analytics-tracking-page-views-etc
   goatCounterCode: null,
